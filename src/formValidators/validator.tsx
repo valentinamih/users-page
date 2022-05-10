325199f9ad0e8a2373ea0443f32762b0df608c51
Valya <vally-arrty@mail.ru>
2022-05-10 17:22:55 +0300

    Updates

--- src/formValidators/validator.tsx
+++
@@ -1,6 +0,0 @@
-export type ValidatorType = (value: string) => string | undefined
-
-export const required: ValidatorType = (value) => {
-    if (value) return undefined;
-    return 'require'
-}
--- src/store/useSelector.ts
+++
@@ -1,4 +0,0 @@
-import {TypedUseSelectorHook, useSelector} from "react-redux";
-import {RootReducerType} from "./store";
-
-export const AppUseSelector: TypedUseSelectorHook<RootReducerType> = useSelector
